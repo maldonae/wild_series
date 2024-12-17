08bf16bbf3b01247a2d0da3c27a252bf2feefd19
maldonae <maldonadoeric69120@gmail.com>
2024-12-17 12:36:11 +0100

    routing_avancé

--- server/src/router.ts
+++ server/src/router.ts
@@ -12,17 +12,23 @@ import itemActions from "./modules/item/itemActions";
 router.get("/api/items", itemActions.browse);
 router.get("/api/items/:id", itemActions.read);
 router.post("/api/items", itemActions.add);
+
+// Define program-related routes
+import programActions from "./modules/program/programActions";
+
 router.get("/api/programs", programActions.browse);
+router.get("/api/programs/:id", programActions.read);
+router.get("/api/categories", categoryActions.browse);
+router.get("/api/categories/:id", categoryActions.read);
 
 /* ************************************************************************* */
 
 // Declaration of a "Welcome" route
-
-import sayActions from "./modules/item/sayActions";
-import programActions from "./modules/program/programActions";
+import categoryActions from "./modules/category/categoryActions";
+import sayActions from "./modules/say/sayActions";
 
 router.get("/", sayActions.sayWelcome);
-router.get("/", programActions.browse);
+
 /* ************************************************************************* */
 
 export default router;
